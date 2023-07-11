2bd073249ace1681eeba3dc7861d66668a5ca261
codemodeactivate <jeff.zenko@gmail.com>
2023-07-11 00:05:56 -0400

    react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hi there!</h1>
+      <Navbar />
     </div>
   );
 }
