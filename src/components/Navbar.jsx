337eac82176241c047821201fa3e462fda5d4733
codemodeactivate <jeff.zenko@gmail.com>
2023-07-12 17:02:10 -0400

    navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import Logo from '../Assets/logo1.png'
+
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
-
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={ Logo } alt="Logo" />
+      </div>
     </div>
   )
 }
