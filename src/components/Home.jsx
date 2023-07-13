d83d7103e0099c6d14880b1caf060ee08e67c27a
codemodeactivate <jeff.zenko@gmail.com>
2023-07-13 12:16:26 -0400

    colors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-teal-1'>
+    <div name='home' className='w-full h-screen bg-teal-4'>
         {/*Container*/}
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
             <p className='text-pink-600'>Hi, my name is</p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ import Mongo from '../Assets/mongo.png';
 
 const Skills = () => {
   return(
-<div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+<div name='skills' className='w-full h-screen bg-yellow-3 text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
