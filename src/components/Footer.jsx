38c5f0a9a95b1beb3f8060dfbb9772770c8fbfd3
codemodeactivate <jeff.zenko@gmail.com>
2023-07-15 23:43:37 -0400

    footer stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { Icon } from '@iconify/react';
+import leetcodeIcon from '@iconify/icons-simple-icons/leetcode';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg-white border-b-4 border-gray-800">
+      <div className="max-w-screen-2xl px-4 md:px-8 mx-auto pt-2">
+        <div className="flex justify-between items-center gap-4 py-8">
+          <span className="text-gray-800 text-lg font-semibold">Jeff Zenko © 2023. All Rights Reserved</span>
+
+          <div className="flex gap-4">
+            <a target="_blank" rel="noreferrer" alt="github" className="text-gray-800 hover:text-gray-500 active:text-gray-600 transition duration-100" href="https://github.com/codemodeactivate">
+            <FaGithub size={30} />
+            </a>
+            <a target="_blank" rel="noreferrer" alt="github" className="text-gray-800 hover:text-gray-500 active:text-gray-600 transition duration-100" href="https://www.linkedin.com/in/jeff-zenko">
+            <FaLinkedin size={30} />
+            </a>
+            <a target="_blank" rel="noreferrer" alt="leetcode" className="text-gray-800 hover:text-gray-500 active:text-gray-600 transition duration-100" href="https://leetcode.com/jzenko/">
+              <Icon icon={leetcodeIcon} width={30} height={30} />
+            </a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,8 +20,8 @@ const Navbar = () => {
         <ul className='md:flex hidden'>
           <li handleClick='home'>Home</li>
           <li >About</li>
-          <li >Resume</li>
           <li >Work</li>
+          <li >Resume</li>
           <li >Contact</li>
         </ul>
 
@@ -37,50 +37,14 @@ const Navbar = () => {
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-teal-3 flex flex-col justify-center items-center'}>
           <li className='py-6 text-4xl'>Home</li>
           <li className='py-6 text-4xl'>About</li>
-          <li className='py-6 text-4xl'>Skills</li>
           <li className='py-6 text-4xl'>Work</li>
+          <li className='py-6 text-4xl'>Resume</li>
           <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
 
 
-        {/* Social Media */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Linkedin <FaLinkedin size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Email <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
-        </ul>
-        </div>
+
       </div>
     </div>
   )
