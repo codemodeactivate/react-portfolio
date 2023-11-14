d715d55731a3bfec474de523bbfaa06ff8c61b25
codemodeactivate <jeff.zenko@gmail.com>
2023-11-14 14:23:54 -0500

    workshare

--- src/components/WorkShare.jsx
+++ src/components/WorkShare.jsx
@@ -15,7 +15,7 @@ const WorkShare = () => {
     // const bg:hover = {
     //     background-image:
     // }
-    const projects = work;
+    // const projects = work;
 
     return (
         <div
@@ -32,49 +32,7 @@ const WorkShare = () => {
                     </div>
 
                     <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-                        {/* Gird Item */}
-                        {projects.map((item, index) => (
-                            <div
-                                key={index}
-                                style={{
-                                    backgroundImage: `url(${item.image})`,
-                                }}
-                                className="max-w-sm border-4 border-black group container
-              flex justify-center text-center items-center mx-auto content-div hover:scale-110"
-                            >
-                                {/* Hover effect for images */}
-                                <div className="opacity-0 group-hover:opacity-100 ">
-                                    <span className="text-2xl font-bold text-gray-800 tracking-wider">
-                                        {item.name}
-                                    </span>
-                                    <div>
-                                    <span className="px-1">{item.description}</span>
-                                    </div>
-                                    <div className="pt-8 text-center ">
-                                        {/* Check if the github property is not an empty string */}
-                {item.github !== "/" && (
-                    <a href={item.github} target="_blank" rel="noreferrer">
-                                            <button
-                                                className="text-center px-4 py-3 m-2 text-gray-800 font-bold
-                                                bg-yellow-1 border-black border-4 hover:bg-yellow-1 hover:border-yellow-1 transition focus:outline-none focus:ring shadow-[6px_6px_0_0_#000000 gap-2 hover:shadow-[6px_6px_0_0_#000] hover:scale-110"
-                                            >
-                                                Code
-                                            </button>
-                                        </a>
-                                        )}
-                                        {/* eslint-disable-next-line */}
-                                        {/* <a href={item.live} target="_blank" rel="noreferrer">
-                                            <button
-                                                className="text-center px-4 py-3 m-2 text-gray-800 font-bold
-                                                bg-yellow-1 border-black border-4 hover:bg-yellow-1 hover:border-yellow-1 transition focus:outline-none focus:ring shadow-[6px_6px_0_0_#000000 gap-2 hover:shadow-[6px_6px_0_0_#000] hover:scale-110"
-                                            >
-                                                Live
-                                            </button>
-                                        </a> */}
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
+                       <img src="src/Assets/pictures/childhood.jpg" alt="childhood"></img>
                     </div>
                 </div>
             </div>
